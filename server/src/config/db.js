946084c155319028dbcf98d42a5f7724f6ba120d
SamvidChawla <samvidchawla15@gmail.com>
2026-03-01 17:32:27 +0530

    DB Update

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -7,6 +7,7 @@ const pool = new Pool({
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
+  ssl: { rejectUnauthorized: false }
 });
 
 export const connectDB = async () => {
